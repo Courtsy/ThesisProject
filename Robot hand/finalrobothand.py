3e39119a4590b7bc65c44a891d392fd894556a6a
Courtsy <tommo500@hotmail.co.uk>
2023-12-12 00:31:11 +0000

    update

--- Robot hand/finalrobothand.py
+++ Robot hand/finalrobothand.py
@@ -1,35 +1,39 @@
-import os
+# Import the necessary libraries.
 import pybullet as p
 import pandas as pd
 import time
 
-# Connect to PyBullet
+# Connect to the PyBullet physics simulation.
 p.connect(p.GUI)
-p.setGravity(0, 0, 0)
+p.setGravity(0, 0, 0) # Set gravity to zero for all axes
 
 
-# Reset the camera for a better view
+# Reset the camera to have a clear view of the simulation
+# Adjust the camera's distance, yaw, pitch, and target position for better visualisation
 p.resetDebugVisualizerCamera(cameraDistance=0.01, cameraYaw=30,
                              cameraPitch=-30, cameraTargetPosition=[0.5, -0.9, 0.75])
 
-# Load the robot hand URDF (use the direct path instead of os.path.join)
+# Load the robot hand URDF file
 robot_id = p.loadURDF(r"C:\Users\tommo\Downloads\simox_ros-master\simox_ros-master\sr_grasp_description\urdf\shadowhand.urdf", useFixedBase=True)
 
-# Read the joint information
+# Create a mapping of joint names to their respective indices within the PyBullet simulation
 joint_name_to_index = {p.getJointInfo(robot_id, i)[1].decode('utf-8'): i for i in range(p.getNumJoints(robot_id))}
 
 
 
-# Load CSV data
+# Load the CSV file containing the formatted data for the hand
 csv_file_path = r'C:\Users\tommo\animation_hand\test2.csv'
 data = pd.read_csv(csv_file_path)
 dataset = data.values.tolist()
 
-# Set the position for each joint according to the CSV file
+# Iterate through each row in the dataset, which corresponds to a set of joint positions
 for data_row in dataset:
+    # Zip together the column names (joint names) and the values in the row (target positions)
     for joint_name, target_position in zip(data.columns, data_row):
+        # If the joint name is recognized in the simulation, find its index
         if joint_name in joint_name_to_index:
             joint_index = joint_name_to_index[joint_name]
+            # Set the joint to move to the target position
             p.setJointMotorControl2(bodyUniqueId=robot_id,
                                     jointIndex=joint_index,
                                     controlMode=p.POSITION_CONTROL,
@@ -39,15 +43,15 @@ for data_row in dataset:
 
 
 
-    # Step the simulation
+    # Step the simulation forward
     p.stepSimulation()
     # Sleep to control the speed of the simulation
     time.sleep(1.0 / 100.0)
 
-# Keep the simulation window open until closed by the user
+# Keep the simulation window open until manually closed
 while p.isConnected():
     p.stepSimulation()
     time.sleep(1.0 / 240.0)
 
-# Disconnect from PyBullet
+# Disconnect from PyBullet once the simulation is closed
 p.disconnect()
